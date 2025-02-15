7259d7dceccaa669e7e4733fc1a68e10ad06d416
Matthew Adebayo <olumatty@gmail.com>
2025-02-15 21:57:50 +0100

    Footer 1.0

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,17 @@ const Footer = () => {
   return (
     <div>
       <div className='py-[60px]'>
-        
+        <div className='w-full max-w-[1440px] mx-auto my-0 px-[20px]'>
+          <div className='flex justify-between gap-x-[60px] md:max-lg:flex-col md:max-lg:gap-y-[60px]'>
+            <div>
+              <div className='flex gap-x-[15px] items-center mb-[30px]'>
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+
       </div>
     </div>
   )
